c20e5032a5c1d52cbc4a0a01382165f9e93482f1
Ernesto Murrugarra <ernestomurrugarra@Ernestos-MBP.fibertel.com.ar>
2023-10-24 15:15:22 -0300

    Added base structure of my site

---
+++ components/web/Sidebar.vue
@@ -0,0 +1,48 @@
+<template>
+  <Transition>
+    <nav
+      v-if="sidebarState"
+      class="w3-sidebar w3-black w3-xxlarge nav"
+      @click="toggleSidebar()"
+    >
+      <a
+        class="w3-button w3-black w3-xxxlarge w3-display-topright close-btn"
+        @click=""
+      >
+        <i class="fa-solid fa-xmark"></i>
+      </a>
+      <div class="w3-bar-block w3-center">
+        <a href="#" class="w3-bar-item w3-button w3-text-grey w3-hover-black">Home</a>
+        <a href="#portfolio" class="w3-bar-item w3-button w3-text-grey w3-hover-black">Portfolio</a>
+        <a href="#about" class="w3-bar-item w3-button w3-text-grey w3-hover-black">About</a>
+        <a href="#contact" class="w3-bar-item w3-button w3-text-grey w3-hover-black">Contact</a>
+      </div>
+    </nav>
+  </Transition>
+</template>
+<script setup lang="ts">
+const { sidebarState, toggleSidebar } = useSidebar()
+</script>
+<style scoped>
+.nav {
+  display: block;
+  width: 60%;
+  padding-top:150px;
+  right:0;
+  z-index:2
+}
+
+.close-btn {
+  padding: 0 12px;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
