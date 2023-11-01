7cf900a91d6681d80474ec166339e2e773375a01
Ernesto Murrugarra <ernestomurrudev@gmail.com>
2023-11-01 13:29:09 -0300

    Added comming soon page

--- app.vue
+++ app.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="wrapper">
+    <p class="icon"><i class="fa-solid fa-code"></i></p>
+    <p class="title"><strong>Murru Dev Website</strong></p>
+    <p class="subtitle">We will be on production soon!</p>
   </div>
 </template>
+<style>
+
+/* Mobile, laptops and large screens rules */
+* { margin: 0 !important; }
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+}
+.icon { font-size: 10em; }
+
+/* Tablet rules */
+@media (min-width: 768px) {
+  .icon { font-size: 15em; }
+  .title { font-size: 2.5em; }
+  .subtitle { font-size: 1.5em; }
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Murru Dev's Site - Coming Soon",
+      link: [
+        { rel: "stylesheet", href: "https://www.w3schools.com/w3css/4/w3.css" }
+      ],
+      script: [
+        { src: "https://kit.fontawesome.com/ea227ca389.js", crossorigin: "anonymous" }
+      ]
+    }
+  }
 })
