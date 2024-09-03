ad1e4c0c85434addef80d6b9c004361659ed2ede
Ernesto Murrugarra <ernestomurrudev@gmail.com>
2024-09-03 18:29:33 -0300

    Added images slider

---
+++ components/ImgCarousel.vue
@@ -0,0 +1,44 @@
+<template>
+  <Carousel :autoplay="5000" :wrap-around="true">
+    <Slide v-for="slide in 5" :key="slide">
+      <div :class="`carousel__item bg-img-base bgimg-${slide}`"></div>
+    </Slide>
+  </Carousel>
+</template>
+<script setup lang="ts"></script>
+<style>
+.carousel,
+.carousel__viewport,
+.carousel__track,
+.carousel__slide {
+  height: 100%;
+}
+
+.bg-img-base {
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 100%;
+  width: 100%;
+}
+
+.bgimg-1 {
+  background-image: url("~/assets/img/slide-1.jpg");
+}
+
+.bgimg-2 {
+  background-image: url("~/assets/img/slide-2.jpg");
+}
+
+.bgimg-3 {
+  background-image: url("~/assets/img/slide-3.jpg");
+}
+
+.bgimg-4 {
+  background-image: url("~/assets/img/slide-4.jpg");
+}
+
+.bgimg-5 {
+  background-image: url("~/assets/img/slide-5.jpg");
+}
+</style>
---
+++ components/Sidebar.vue
@@ -0,0 +1,79 @@
+<template>
+  <Transition>
+    <nav
+      v-if="sidebarState"
+      class="w3-sidebar w3-black w3-xxlarge nav"
+      @click="toggleSidebar()"
+    >
+      <a
+        class="w3-button w3-black w3-xxxlarge w3-display-topright close-btn"
+        @click=""
+      >
+        <i class="fa-solid fa-xmark"></i>
+      </a>
+      <div class="w3-bar-block w3-center">
+        <a
+          href="#home"
+          class="w3-bar-item w3-button w3-text-grey w3-hover-black"
+          >Home</a
+        >
+        <a
+          href="#reputation"
+          class="w3-bar-item w3-button w3-text-grey w3-hover-black"
+          >Reputation</a
+        >
+        <a
+          href="#about"
+          class="w3-bar-item w3-button w3-text-grey w3-hover-black"
+          >About</a
+        >
+        <a
+          href="#stats"
+          class="w3-bar-item w3-button w3-text-grey w3-hover-black"
+          >Stats</a
+        >
+        <a
+          href="#portfolio"
+          class="w3-bar-item w3-button w3-text-grey w3-hover-black"
+          >Portfolio</a
+        >
+        <a
+          href="#demos"
+          class="w3-bar-item w3-button w3-text-grey w3-hover-black"
+          >Demos</a
+        >
+        <a
+          href="#tutorials"
+          class="w3-bar-item w3-button w3-text-grey w3-hover-black"
+          >Tutorials</a
+        >
+      </div>
+    </nav>
+  </Transition>
+</template>
+<script setup lang="ts">
+const { sidebarState, toggleSidebar } = useSidebar();
+</script>
+<style scoped>
+.nav {
+  display: block;
+  width: 60%;
+  padding-top: 150px;
+  right: 0;
+  z-index: 2;
+}
+
+.close-btn {
+  padding: 0 12px;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
